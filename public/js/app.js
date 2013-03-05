59938ede352c85b9f2b2823d596f704839f7bc29
Olivier Penhoat <openhoat@gmail.com>
2013-03-05 18:33:04 +0100

    add missing files

---
+++ public/js/app.js
@@ -0,0 +1,167 @@
+$(function () {
+  var socket;
+
+  function StringBuffer() {
+    this.buffer = [];
+  }
+
+  StringBuffer.prototype.append = function append(string) {
+    this.buffer.push(string);
+    return this;
+  };
+  StringBuffer.prototype.toString = function toString() {
+    return this.buffer.join('');
+  };
+
+  function truncateTweets() {
+    var maxTweets = $('#maxTweets').val();
+    while ($('#tweets > ul > li').length > maxTweets) {
+      $('#tweets > ul > li:last').remove();
+    }
+  }
+
+  function notify(title, msg) {
+    if (title) {
+      $('#notifyContainer .title').text(title);
+    }
+    $('#notifyContainer .message').text(msg);
+    $('#notifyContainer > .alert').show();
+    window.setTimeout(function () {
+      $('#notifyContainer > .alert').hide();
+    }, 2000);
+  }
+
+  function init() {
+    $('#notifyContainer .close').click(function () {
+      $('#notifyContainer > .alert').hide();
+    });
+    $('#criteriaForm').submit(function () {
+      socket.emit('criteria', {
+        place:$('#place').val(),
+        distance:$('#distance').val(),
+        keywords:$('#keywords').val()
+      });
+      socket.emit('tweetStream', false);
+      notify('Info', 'Change done.');
+      return false;
+    });
+    $('#streamingSwitch').on('switch-change', function (e, data) {
+      var active = data.value;
+      if (active) {
+        $('#tweets > ul').empty();
+        socket.emit('tweetStream', true);
+        notify('Info', 'Waiting for new tweets...');
+      } else {
+        socket.emit('tweetStream', false);
+        notify('Info', 'Tweets streaming stopped.');
+      }
+    });
+    $('#maxTweets').change(function () {
+      truncateTweets();
+      return false;
+    });
+  }
+
+  function initTracking() {
+    var watchId = null;
+    if (!navigator.geolocation) {
+      $('#trackingControlGroup').hide();
+      socket.emit('location');
+      return false;
+    }
+    $('#trackingSwitch').on('switch-change', function (e, data) {
+      var fields = $('#critertiaFields input, #critertiaFields button')
+        , active = data.value;
+      if (active) {
+        fields.attr('disabled', 'disabled');
+        watchId = navigator.geolocation.watchPosition(function (position) {
+          socket.emit('location', {
+            longitude:position.coords.longitude,
+            latitude:position.coords.latitude
+          });
+        }, function (error) {
+          socket.emit('location');
+        }, {enableHighAccuracy:true, maximumAge:30000, timeout:27000});
+      } else {
+        fields.removeAttr('disabled', 'disabled');
+        if (watchId !== null) {
+          navigator.geolocation.clearWatch(watchId);
+          watchId = null;
+        }
+      }
+    });
+    navigator.geolocation.getCurrentPosition(function (position) {
+      socket.emit('location', {
+        longitude:position.coords.longitude,
+        latitude:position.coords.latitude
+      });
+    }, function (error) {
+      socket.emit('location');
+    });
+    return true;
+  }
+
+  function initSocket() {
+    socket = io.connect(window.location.origin);
+
+    socket.on('transport', function(transport){
+      console.log('web sockets transport mode :', transport);
+    });
+
+    socket.on('client', function (client) {
+      $('#longitude').text(client.location.longitude || '');
+      $('#latitude').text(client.location.latitude || '');
+      $('#number').text(client.address.number || '');
+      $('#street').text(client.address.street || '');
+      $('#city').text(client.address.city || '');
+      $('#region').text(client.address.region || '');
+      $('#country').text(client.address.country || '');
+      $('#place').val((client.location.longitude + ',' + client.location.latitude) || '');
+    });
+
+    socket.on('tweet', function (tweet) {
+      var buffer = new StringBuffer()
+        , content
+        , noTweets = $('#tweets > ul > li').length;
+      buffer.append('<li>');
+      buffer.append('<div class="image">');
+      buffer.append('<img src="');
+      buffer.append(tweet.user.image);
+      buffer.append('">');
+      buffer.append('</div>');
+      buffer.append('<div class="content">');
+      buffer.append('<div class="header">');
+      buffer.append('<span class="account">@');
+      buffer.append(tweet.user.account);
+      buffer.append('</span>');
+      buffer.append(' ');
+      buffer.append('<span class="name">');
+      buffer.append(tweet.user.name);
+      buffer.append('</span>');
+      buffer.append(' ');
+      buffer.append('<div class="created">');
+      buffer.append(tweet.created);
+      buffer.append('</div>');
+      buffer.append('</div>');
+      buffer.append('<p class="text">');
+      buffer.append(tweet.text);
+      buffer.append('</p>');
+      buffer.append('</div>');
+      buffer.append('<div class="location clear">');
+      buffer.append(tweet.user.location);
+      buffer.append('</div>');
+      buffer.append('</li>');
+      content = buffer.toString();
+      if (noTweets) {
+        $(content).insertBefore('#tweets > ul > li:first-child');
+      } else {
+        $('#tweets > ul').append(content);
+      }
+      truncateTweets();
+    });
+  }
+
+  init();
+  initTracking();
+  initSocket();
+});
